7026707a982d7b59f8f5ac19fdeefa925065bef7
Sehyo <Alex_Noble@hotmail.com>
2017-01-19 02:59:33 +0000

    Added class to represent sequences
    Added class to represent sequences

---
+++ Sequence.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Sequence.h"
+
+
+Sequence::Sequence(int team, std::vector<int> indices)
+{
+	this->indices = indices;
+}
+
+Sequence::~Sequence()
+{}
---
+++ Sequence.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+class Sequence
+{
+public:
+	int team; // What team the sequence represents.
+	std::vector<int> indices; // what indices in the board vector this sequence is made of.
+	Sequence(int team, std::vector<int> indices);
+	~Sequence();
+};
+
